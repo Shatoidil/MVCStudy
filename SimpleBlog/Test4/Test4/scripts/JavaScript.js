2474455c9921cc817c67bf7c01e56db502410bda
Shato Daltsaev <shatoidil@gmail.com>
2016-03-07 23:11:31 +0300

    Тест Asp MVC and partial jquery

---
+++ SimpleBlog/Test4/Test4/scripts/JavaScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function() {
+    $('.sex').each(function () {
+        switch ($(this).html()) {
+            case '0':
+                $(this).html('Женщина');
+                break;
+            case '1':
+                $(this).html('Мужчина');
+                break;
+            default:
+                $(this).html('Не определен');
+                break;
+        }
+    });
+
+});
