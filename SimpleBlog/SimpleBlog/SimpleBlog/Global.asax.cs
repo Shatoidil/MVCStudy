6aa070e5f60ac1838a4404ce2addedee0b8f9b14
Shato Daltsaev <shatoidil@gmail.com>
2016-03-09 22:49:26 +0300

    Imported styles and scripts 12

--- SimpleBlog/SimpleBlog/SimpleBlog/Global.asax.cs
+++ SimpleBlog/SimpleBlog/SimpleBlog/Global.asax.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+
+
+using SimpleBlog.App_Start;
 
 namespace SimpleBlog
 {
@@ -12,7 +16,9 @@ namespace SimpleBlog
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+           
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
