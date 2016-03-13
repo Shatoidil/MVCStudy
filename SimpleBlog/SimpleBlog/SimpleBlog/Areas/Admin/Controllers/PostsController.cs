915ab2a7bbf578bdb196a723f096f86051e35aa3
Shato Daltsaev <shatoidil@gmail.com>
2016-03-13 13:47:14 +0300

    13 Lection/ Admin Layout

--- SimpleBlog/SimpleBlog/SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/SimpleBlog/SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,16 +1,18 @@
 ﻿
+using SimpleBlog.Infrastructure;
 using System.Web.Mvc;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
         //Дает по роли админа доступ в этот раздел
         
         public ActionResult Index()
         {
-            return Content("Admins Posts!");
+            return View();
         }
     }
 }
---
+++ SimpleBlog/SimpleBlog/SimpleBlog/Infrastructure/SelectedTabAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class SelectedTabAttribute : ActionFilterAttribute
+    {
+        private readonly string _sectedTab;
+
+        public SelectedTabAttribute(string selectedTab)
+        {
+            _sectedTab = selectedTab; 
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.SelectedTab = _sectedTab;
+        }
+    }
+}
