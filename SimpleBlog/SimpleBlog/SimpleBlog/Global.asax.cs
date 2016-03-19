29a1f1f8b9401a50df20f11023584f356450d007
Shato Daltsaev <shatoidil@gmail.com>
2016-03-19 21:13:05 +0300

    lection 18

--- SimpleBlog/SimpleBlog/SimpleBlog/Global.asax.cs
+++ SimpleBlog/SimpleBlog/SimpleBlog/Global.asax.cs
@@ -19,6 +19,15 @@ namespace SimpleBlog
            
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.Configure();
+        }
+        protected void Application_BeginRequest()
+        {
+            Database.OpenSession();
+        }
+        protected void Application_EndRequest()
+        {
+            Database.CloseSession();
         }
     }
 }
